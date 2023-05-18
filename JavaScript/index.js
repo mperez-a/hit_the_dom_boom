5ec5b455a4e71907f66700555769e26f3d730571
Manuela Perez <manuelaperezag@gmail.com>
2023-05-18 14:55:21 +0200

    desktop version finished

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,16 +5,3 @@ document.querySelectorAll('.drumkit-container button').forEach(button => {
 		sound.play();
 	});
 });
-
-const hamburger_menu = document.querySelector(".hamburger-menu");
-const nav_menu = document.querySelector(".nav-bar");
-
-hamburger_menu.addEventListener('click', () => {
-	hamburger_menu.classList.toggle("active");
-	nav_menu.classList.toggle("active");
-});
-
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener('click', () => {
-	hamburger_menu.classList.remove("active");
-	nav_menu.classList.remove("active");
-}));
